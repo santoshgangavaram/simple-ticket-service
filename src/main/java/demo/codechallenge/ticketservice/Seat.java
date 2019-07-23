af213a9565fdcf65c0c7d90abaef0374c0b0bfec
santoshgangavaram <santosh.gangavaram@gmail.com>
2019-07-23 09:32:32 -0700

    added equals and hashcode methods to Seat class

--- src/main/java/demo/codechallenge/ticketservice/Seat.java
+++ src/main/java/demo/codechallenge/ticketservice/Seat.java
@@ -81,4 +81,23 @@ public class Seat {
 		        + this.column
 		        + '}';
   }
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + column;
+    result = prime * result + row;
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (getClass() != obj.getClass()) return false;
+    Seat other = (Seat) obj;
+    if (column != other.column) return false;
+    if (row != other.row) return false;
+    return true;
+  }
 }
